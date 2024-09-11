bf6edc792f250d1afc52bf6796d433139ee44326
theoldtree <yjeongmu@gmail.com>
2024-09-11 17:18:58 +0900

    login page layout

--- src/app/page.js
+++ src/app/page.js
@@ -15,20 +15,16 @@ export default function Home() {
         </div>
         <div className="flex flex-col items-center sm:items-start gap-6">
           {" "}
-          {/* 간격을 더 넓게 설정 */}
-          {/* Username Input */}
           <input
             type="text"
             placeholder="Username"
             className="w-full sm:w-80 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-          {/* Password Input */}
           <input
             type="password"
             placeholder="Password"
             className="w-full sm:w-80 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-          {/* Login Button */}
           <button className="w-full sm:w-80 px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 transition-colors focus:outline-none focus:ring-2 focus:ring-indigo-400">
             Login
           </button>
