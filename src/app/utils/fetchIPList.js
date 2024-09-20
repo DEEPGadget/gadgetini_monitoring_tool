0c3cc798f25ad2dd55af93310268626526e8cd3f
theoldtree <yjeongmu@gmail.com>
2024-09-20 09:11:24 +0900

    fetch ip list file add

---
+++ src/app/utils/fetchIPList.js
@@ -0,0 +1,13 @@
+// /utils/fetchIPList.js
+
+export const fetchIPList = async () => {
+  try {
+    const response = await fetch("/api/ip");
+    if (!response.ok) throw new Error("Network response was not ok.");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch iplist:", error);
+    return [];
+  }
+};
